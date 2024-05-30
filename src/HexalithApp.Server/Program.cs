fec7ccb06b48312685b054ba1d5caacf8783ea12
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-05-30 15:56:41 +0200

    fix issues

--- src/HexalithApp.Server/Program.cs
+++ src/HexalithApp.Server/Program.cs
@@ -1,12 +1,8 @@
 namespace HexalithApp.Server;
 
-using Hexalith.Application.Modules.Modules;
 using Hexalith.Infrastructure.ClientAppOnServer.Helpers;
 
-using HexalithApp.Client.Modules;
 using HexalithApp.Server.Components;
-using HexalithApp.Server.Modules;
-using HexalithApp.Shared.Modules;
 
 public static class Program
 {
@@ -18,70 +14,9 @@ public static class Program
             "1.0.0",
             registerActors: _ => { },
             args);
-        _ = builder.Services.AddSingleton<IApplicationModule, HexalithSharedModule>();
-        _ = builder.Services.AddSingleton<IApplicationModule, HexalithClientModule>();
-        _ = builder.Services.AddSingleton<IApplicationModule, HexalithServerModule>();
-
-        //// Add services to the container.
-        // _ = builder.Services.AddRazorComponents()
-        //    .AddInteractiveServerComponents()
-        //    .AddInteractiveWebAssemblyComponents();
-        // _ = builder.Services.AddFluentUIComponents();
-
-        // _ = builder.Services.AddCascadingAuthenticationState();
-        // _ = builder.Services.AddScoped<IdentityUserAccessor>();
-        // _ = builder.Services.AddScoped<IdentityRedirectManager>();
-        // _ = builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
-
-        // _ = builder.Services.AddAuthentication(options =>
-        //    {
-        //        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        //        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-        //    })
-        //    .AddIdentityCookies();
-
-        // string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        // _ = builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        //    options.UseSqlServer(connectionString));
-        // _ = builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        // _ = builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-        //    .AddEntityFrameworkStores<ApplicationDbContext>()
-        //    .AddSignInManager()
-        //    .AddDefaultTokenProviders();
-
-        // _ = builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
         WebApplication app = builder.Build();
-        _ = app.UseHexalithWebApplication<_Imports>();
+        _ = app.UseHexalithWebApplication<App>();
 
-        //// Configure the HTTP request pipeline.
-        // if (app.Environment.IsDevelopment())
-        // {
-        //    app.UseWebAssemblyDebugging();
-        //    _ = app.UseMigrationsEndPoint();
-        // }
-        // else
-        // {
-        //    _ = app.UseExceptionHandler("/Error");
-
-        // // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        //    _ = app.UseHsts();
-        // }
-
-        // _ = app.MapSwagger();
-
-        // _ = app.UseHttpsRedirection();
-
-        // _ = app.UseStaticFiles();
-        // _ = app.UseAntiforgery();
-
-        // _ = app.MapRazorComponents<App>()
-        //    .AddInteractiveServerRenderMode()
-        //    .AddInteractiveWebAssemblyRenderMode()
-        //    .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
-
-        //// Add additional endpoints required by the Identity /Account Razor components.
-        // _ = app.MapAdditionalIdentityEndpoints();
         app.Run();
     }
 }
