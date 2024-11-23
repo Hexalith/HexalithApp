8ba9e7c841062c88b1a566576bad732d79347b2e
Jérôme Piquot <jpiquot@itaneo.com>
2024-11-23 12:17:04 +0100

    Refactor identity management to use Dapr Identity Store
    Significant changes include:
    - Added new properties `HexalithDaprIdentityStorePath` and `HexalithDaprIdentityStoreVersion` to `Directory.Build.props` and `Hexalith.props`.
    - Updated submodule commits for `Hexalith` and `Hexalith.DaprIdentityStore`.
    - Added new project references in `Hexalith.Security.sln` and `Hexalith.Security.ApiServer.csproj`.
    - Included `Hexalith.DaprIdentityStore.Helpers` in `HexalithSecurityApiServerModule.cs` and registered identity actors.
    - Removed `ApplicationUser` class and related identity management code.
    - Deleted several Razor components and classes related to identity management.
    - Removed various Razor components related to user account management.
    - Refactored `Hexalith.Security.WebServer.csproj` and `HexalithSecurityWebServerModule.cs` to use Dapr Identity Store UI components.
    - Added new classes and methods for Dapr Identity Store integration.
    - Updated project files and namespaces to reflect changes.

--- src/HexalithApp.WebServer/Program.cs
+++ src/HexalithApp.WebServer/Program.cs
@@ -3,8 +3,6 @@ namespace HexalithApp.WebServer;
 using Hexalith.Application.Modules.Applications;
 using Hexalith.Infrastructure.ClientAppOnServer.Helpers;
 
-using HexalithApp.WebServer.Components.Pages;
-
 /// <summary>
 /// The entry point of the application.
 /// </summary>
@@ -20,6 +18,7 @@ public static class Program
         IWebServerApplication application = HexalithApplication.WebServerApplication ?? throw new InvalidOperationException("WebServerApplication is not defined");
         WebApplicationBuilder builder = ServerSideClientAppHelper.CreateServerSideClientApplication(
             application.Name,
+            (c) => { },
             application.SessionCookieName,
             "1.0.0",
             args);
