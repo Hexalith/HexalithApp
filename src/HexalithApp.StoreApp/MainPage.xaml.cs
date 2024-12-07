55e20a1324b7bfc13d1006f40f479b2ed47dc41d
Jérôme Piquot <jpiquot@itaneo.com>
2024-12-07 16:57:17 +0100

    Remove .NET MAUI project and refactor authentication
    Significant changes include:
    - Marked subproject commits in `Hexalith`, `Hexalith.Security`, and `HexalithApp` as dirty.
    - Updated `Hexalith.Documents.sln` to add `SolutionItems` and remove properties files from `_build`.
    - Added new `using` directives in `DocumentAdd.razor`.
    - Updated `ConfigureSettings` in `OptionsHelper.cs` to include a new constraint and ensure validation on start.
    - Updated `GetSettings` in `SettingsHelper.cs` to throw `InvalidOperationException` if the configuration section is not found.
    - Optimized `OptionsBuilder.cs` by using `configurationName` variable.
    - Changed company name and updated copyright in `.stylecop.json`.
    - Updated package versions in `packages.lock.json`.
    - Removed several files related to `HexalithApp.StoreApp` project, indicating a significant cleanup or refactor.
    - Deleted all configurations, dependencies, and properties related to the .NET MAUI project in `HexalithApp.StoreApp.csproj`.
    - Removed platform-specific files and resources for Android, iOS, Tizen, and WinUI.
    - Removed `dotnet_bot.svg` path elements, `AboutAssets.txt`, `splash.svg`, and `AuthenticatedUser.cs`.
    - Removed `ExternalAuthStateProvider.cs`, `favicon.png`, and `index.html`, indicating a refactor of authentication and main entry point.

--- src/HexalithApp.StoreApp/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HexalithApp.StoreApp;
-
-public partial class MainPage : ContentPage
-{
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MainPage"/> class.
-    /// </summary>
-    public MainPage() => InitializeComponent();
-}
