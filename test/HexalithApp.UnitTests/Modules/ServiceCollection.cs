40f4588265adff3d773d5ad458a06832bc1995a2
Jérôme Piquot <jpiquot@itaneo.com>
2025-06-25 14:47:37 +0200

    fix: update copyright notices and suppress warnings
    - Updated `NoWarn` property in `Directory.Build.props` to include `RCS1093` for enhanced warning suppression.
    - Added copyright notice in `ServiceCollection.cs` for ITANEO, ensuring proper attribution and licensing.
    - Updated copyright notice in `Usings.cs` to reflect ITANEO ownership and retained warning suppression directive.

--- test/HexalithApp.UnitTests/Modules/ServiceCollection.cs
+++ test/HexalithApp.UnitTests/Modules/ServiceCollection.cs
@@ -1,7 +1,10 @@
-﻿namespace HexalithApp.UnitTests.Modules;
+﻿// <copyright file="ServiceCollection.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace HexalithApp.UnitTests.Modules;
 
 using Microsoft.Extensions.DependencyInjection;
 
-internal class ServiceCollection : List<ServiceDescriptor>, IServiceCollection
-{
-}
+internal class ServiceCollection : List<ServiceDescriptor>, IServiceCollection;
